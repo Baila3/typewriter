c26a13f6fe9015099e59bab512a5912772983cd7
Baila3 <bailaly16@gmail.com>
2022-04-12 13:44:01 -0400

    animated string

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs \n";
+let count = 0
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char)
+}, count) 
+count += 50 
+}
+
+
+
+
+
+
+
